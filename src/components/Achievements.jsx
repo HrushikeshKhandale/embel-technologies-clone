0c7e187498f6b24dffab9ea9b0d2a9e888b0ad81
Hrushikesh <khandalehrushikesh2@gmail.com>
2024-06-04 20:01:02 +0530

    Achievement component created

---
+++ src/components/Achievements.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../App.css";
+
+const Achievements = () => {
+  return (
+    <>
+      <div className="achievement" >
+        <ul>
+          <li>200 <span> Our Customer</span></li>
+          <li>508 <span>Working Hours</span></li>
+          <li>300 <span>Happy Clients</span></li>
+          <li>20 <span>Award Received</span></li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Achievements;
